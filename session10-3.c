f7b7d9d626670123bb6f6afb1d3e17f729dc5acf
Thiencc03-07-2006 <cct20062250.@gmail.com>
2024-12-03 09:48:57 +0700

    Update session10-3.c

--- session10-3.c
+++ session10-3.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 int main(){
-	int arr[5]={4,5,3,2,1},i=0,j,safe;
+	int arr[5]={4,5,3,2,1},i=0,j,save;
 	while(i<5){
-		safe=arr[i];
+		save=arr[i];
 		j=i-1;
-		while(j>=0&&arr[j]>safe){
+		while(j>=0&&arr[j]>save){
 			arr[j+1]=arr[j];
 			j=j-1;
 		}
-		arr[j+1]=safe;
+		arr[j+1]=save;
 		i++;
 	}
 	i=0;
